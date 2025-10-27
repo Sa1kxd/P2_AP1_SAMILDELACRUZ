10715885a7cede5d4581b747ae65f31e41eacc70
Sa1kxd <samildelacruz1@gmail.com>
2025-10-27 18:41:15 -0400

    Contexto creado

---
+++ P2_AP1_SAMILDELACRUZ/DAL/Contexto.cs
@@ -0,0 +1,8 @@
+﻿namespace P2_AP1_SAMILDELACRUZ.DAL;
+using Microsoft.EntityFrameworkCore;
+
+public class Contexto : DbContext
+{
+    public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+
+}

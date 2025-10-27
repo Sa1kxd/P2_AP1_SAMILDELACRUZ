07caaa022144c4bfd20cbd6c822c18f12565be68
Sa1kxd <samildelacruz1@gmail.com>
2025-10-27 18:45:02 -0400

    Creacion del modelo y Dbset configurado dentro del contexto

--- P2_AP1_SAMILDELACRUZ/DAL/Contexto.cs
+++ P2_AP1_SAMILDELACRUZ/DAL/Contexto.cs
@@ -1,8 +1,10 @@
 ﻿namespace P2_AP1_SAMILDELACRUZ.DAL;
 using Microsoft.EntityFrameworkCore;
+using P2_AP1_SAMILDELACRUZ.Models;
 
 public class Contexto : DbContext
 {
     public Contexto(DbContextOptions<Contexto> options) : base(options) { }
 
+    public DbSet<Registro> Registro { get; set; }
 }
---
+++ P2_AP1_SAMILDELACRUZ/Models/Registro.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace P2_AP1_SAMILDELACRUZ.Models;
+
+public class Registro
+{
+    [Key]
+    public int RegistroId { get; set; }
+}
